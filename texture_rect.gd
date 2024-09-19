853c583757e7c4543270cc2d11ad76ea771fee46
sethblocks <56612770+sethblocks@users.noreply.github.com>
2024-09-18 20:39:12 -0700

    Upload Source Code

---
+++ texture_rect.gd
@@ -0,0 +1,66 @@
+extends TextureRect
+
+
+
+var about = "SKEdit, Official Project Shopkeeper Level Maker. Version 0.1 Alpha"
+var version = "0.1"
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#self.scale = Vector2(0.75,0.75);
+	self.position = (get_viewport_rect().size / 2) - self.size /2;
+	
+var bloblos = [];
+var tracking = [];
+var spwn = Vector2(0,0);
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_undo"):
+		bloblos.remove_at(len(bloblos)-1)
+		if len(tracking) !=0:
+			tracking[len(tracking)-1].position = Vector2(-1000,-1000);
+			tracking[len(tracking)-1].queue_free()
+			tracking.remove_at(len(tracking)-1)
+		
+
+
+func _on_text_edit_text_changed(): # BG
+	var img = Image.new();
+	img.load(get_node("../Label/TextEdit").text);
+	var imt = ImageTexture.new();
+	imt.set_image(img);
+	get_node("../TextureRect").texture = imt;
+
+
+func clickpoints(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed == false:
+			if event.button_index==1:
+				var bloblo = get_node("bloblo").duplicate();
+				bloblo.position = event.position;
+				bloblos.append(event.position);
+				
+				bloblo.get_node("Label").text = str(len(bloblos));
+				tracking.append(bloblo);
+				get_node("..").add_child(tracking[len(tracking)-1]);
+			else:
+				spwn = event.position
+				get_node("../bloblo2").position = event.position
+		
+
+
+func save():
+	var savedata = {
+		"title": "MapEditor",
+		"desc": "Level Made in Skedit Ver. " + version,
+		"points": bloblos,
+		"Background": get_node("../Label/TextEdit").text
+	};
+	print(savedata)
+	var mfile = FileAccess.open(get_node("../Label2/TextEdit").text, FileAccess.WRITE)
+	var datstr = JSON.stringify(savedata);
+	mfile.store_line(datstr)
